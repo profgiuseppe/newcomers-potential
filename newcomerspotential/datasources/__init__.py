41b6d421f4ae586455d7a3854af2ad8beea98880
Giuseppe Profiti <giuseppe.profiti@wikimedia.it>
2017-08-10 15:03:56 -0400

    init file

---
+++ newcomerspotential/datasources/__init__.py
@@ -0,0 +1,16 @@
+"""
+This module implements a set of
+:class:`~newcomerspotential.Datasource`
+processors that represent the input data for extracting
+
+Base classes
+++++++++++++
+.. automodule:: newcomerspotential.datasources.datasource
+
+
+
+
+"""
+from .datasource import Datasource
+
+__all__ = [Datasource]
